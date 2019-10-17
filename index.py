2ff8a584860a6d0faae336e6071e72669c8b0b72
Edison Un <chonwaiun@gmail.com>
2019-10-17 12:03:15 +0800

    Add: Add the Random Forest algorithm.

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
 import os
+import numpy as np
 from service import GetFeature as GetFeature
+from service import TrainModel_RandomForest as TestTrain
+from service import Utility as Utility
 
 def main():
-    GetFeature.getFeature('./data/trian_po_set3298_for_ampep_sever.fasta', './data/trian_po_set3298_for_ampep_sever.tsv', 'AAC')
-
+    GetFeature.getFeature('./data/trian_po_set3298_for_ampep_sever.fasta', './data/trian_po_set3298_for_ampep_sever.tsv', 'CTDC')
+    GetFeature.getFeature('./data/trian_ne_set9894_for_ampep_sever.fasta', './data/trian_ne_set9894_for_ampep_sever.tsv', 'CTDC')
+    utility = Utility.Utility('Train')
+    posArray, posY = utility.readFeature("data/trian_po_set3298_for_ampep_sever.tsv", 1)
+    negArray, negY = utility.readFeature("data/trian_ne_set9894_for_ampep_sever.tsv", 0)
+    X = np.concatenate((posArray, negArray))
+    y = np.concatenate((posY, negY))
+    print(len(X), len(y))
+    TestTrain.testTrain(X, y)
+    
 main()
---
+++ service/TrainModel_RandomForest.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+
+def testTrain(X = [], y = []):
+    fold = 10
+    clf = RandomForestClassifier(n_estimators=100)
+    score = cross_val_score(clf, X, y, cv=fold)
+    print(score)
+    meanAcc = 0
+    for i in score:
+        meanAcc = meanAcc + i
+    meanAcc = (meanAcc / fold) * 100
+    print(meanAcc)
