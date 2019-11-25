05bac0edc2dee9bba3d24992e2399431b4a1a1a5
Chonwai Un <chonwaiun@outlook.com>
2019-11-25 16:41:16 +0800

    Add: Add the different version validation function.

--- service/Router.py
+++ service/Router.py
@@ -4,4 +4,8 @@ class Router():
     def __init__(self, method = ''):
         self.method = method
     def randomForest(self, X, y, fold = 10, trees = 100):
-        TrainRandomForest.testTrain(X, y, fold, trees)
+        # TrainRandomForest.testTrain(X, y, fold, trees)
+        # TrainRandomForest.stratifiedKFold(X=X, Y=y)
+        # TrainRandomForest.kFold(X=X, Y=y)
+        TrainRandomForest.stratifiedShuffleSplit(X=X, Y=y)
+        # TrainRandomForest.repeatedStratifiedKFold(X=X, Y=y)
--- service/TrainModel_RandomForest.py
+++ service/TrainModel_RandomForest.py
@@ -1,7 +1,12 @@
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
+from sklearn.model_selection import permutation_test_score
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import RepeatedStratifiedKFold
 
 def testTrain(X = [], y = [], fold = 10, trees = 100):
     clf = RandomForestClassifier(n_estimators=trees)
@@ -13,3 +18,27 @@ def testTrain(X = [], y = [], fold = 10, trees = 100):
         meanAcc = meanAcc + i
     meanAcc = (meanAcc / fold) * 100
     print(meanAcc)
+
+def stratifiedKFold(fold=10, X=[], Y=[]):
+    rf = RandomForestClassifier(n_estimators=100)
+    cv = StratifiedKFold(fold)
+    score, permutation_scores, pvalue = permutation_test_score(rf, X, Y, scoring="accuracy", cv=cv)
+    print("Classification score %s (pvalue : %s) %s" % (score * 100, pvalue, permutation_scores))
+
+def kFold(fold=10, X=[], Y=[]):
+    rf = RandomForestClassifier(n_estimators=100)
+    cv = KFold(fold)
+    score, permutation_scores, pvalue = permutation_test_score(rf, X, Y, scoring="accuracy", cv=cv)
+    print("Classification score %s (pvalue : %s) %s" % (score * 100, pvalue, permutation_scores))
+
+def stratifiedShuffleSplit(fold=10, X=[], Y=[]):
+    rf = RandomForestClassifier(n_estimators=100)
+    cv = StratifiedShuffleSplit(fold)
+    score, permutation_scores, pvalue = permutation_test_score(rf, X, Y, scoring="accuracy", cv=cv, n_permutations=10)
+    print("Classification score %s (pvalue : %s) %s" % (score * 100, pvalue, permutation_scores))
+
+def repeatedStratifiedKFold(fold=10, X=[], Y=[]):
+    rf = RandomForestClassifier(n_estimators=100)
+    cv = RepeatedStratifiedKFold(fold)
+    score, permutation_scores, pvalue = permutation_test_score(rf, X, Y, scoring="accuracy", cv=cv, n_permutations=10)
+    print("Classification score %s (pvalue : %s) %s" % (score * 100, pvalue, permutation_scores))
